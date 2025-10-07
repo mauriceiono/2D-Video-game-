96b62ec87113c8fd00c5a904aa6570e6fb82dbaa
MattBojanowski <111985617+MattBojanowski@users.noreply.github.com>
2025-10-07 19:07:03 -0400

    Update PlayerMovement.cs
    Added more comments to make the code easier to understand.

--- 2dGame/Assets/Scripts/PlayerMovement.cs
+++ 2dGame/Assets/Scripts/PlayerMovement.cs
@@ -10,21 +10,26 @@ public class PlayerMovement2D : MonoBehaviour
     private Vector2 movementInput;
     private Camera mainCamera;
 
+    //Assigned in inspector.
+    //The prefab for the boomerang projectile.
     public GameObject boomerangPrefab;
 
+    //Assigned in inspector.
+    //Prefab for the basic attack projectile.
     public GameObject basicProjectilePrefab;
 
     //Set to default projectiles shoot interval because basic attack is the starting one
     public float shootInterval = 0.6f;
     public Transform shootPoint;
 
+    //Constantly goes up. Once it equals shootInterval, the projectile for the attack is shot and this resets to 0.
     private float shootTimer = 0f;
 
     //Basic attack is the default starting attack
     private string attackType = "Basic";
 
 
-    //These will only get changed during balance patches
+    //These will only get changed during balance patches, no need to make functions for these.
     public float basicProjectileShootInterval = 0.3f;
     public float boomerangShootInterval = 1f;
 
@@ -34,7 +39,10 @@ public class PlayerMovement2D : MonoBehaviour
         shootTimer = 0f;
         mainCamera = Camera.main;
         rb = GetComponent<Rigidbody2D>();
+
+        //Player starts with basic attack when starting the game
         attackType = "Basic";
+        //The basic attack has less damage but shoots faster than the boomerang, so its shoot interval is changed accordingly.
         SetShootInterval(basicProjectileShootInterval);
     }
 
@@ -42,13 +50,19 @@ public class PlayerMovement2D : MonoBehaviour
     {
         //Makes the player follow the mouse cursor for its movement
         playerMoving();
+
+        //Press E to swap weapons. This should be deleted later after a power ups system is made.
         if (Input.GetKeyDown(KeyCode.E))
         {
+
+            //If using basic attack swap to boomerang attack
             if (attackType == "Basic")
             {
                 attackType = "Boomerang";
                 SetShootInterval(boomerangShootInterval);
             }
+
+            //If using boomerang attack, swap to basic attack
             else if (attackType == "Boomerang")
             {
                 attackType = "Basic";
@@ -56,14 +70,20 @@ public class PlayerMovement2D : MonoBehaviour
             }
         }
 
+        //Shoot timer goes up
         shootTimer += Time.deltaTime;
 
+        //Player is ready to attack
         if (shootTimer >= shootInterval)
         {
+
+            //If using a basic attack, shoot the basic projectile
             if (attackType == "Basic")
             {
                 attack(basicProjectilePrefab);
             }
+
+            //If using boomerang attack, shoot a boomerang
             else if (attackType == "Boomerang")
             {
                 attack(boomerangPrefab);
@@ -112,6 +132,7 @@ public class PlayerMovement2D : MonoBehaviour
         //SetPlayerPosition(new Vector2(worldPos.x + 4f, worldPos.y));
     }
 
+    //More getters and setters
     public void SetAttackTimer(float newSpeed)
     {
         shootTimer = newSpeed;
@@ -132,16 +153,22 @@ public class PlayerMovement2D : MonoBehaviour
         shootInterval = newInterval;
     }
 
+    //Creates a projectile for an attack.
     public void attack(GameObject projectileType)
     {
+
+        //Makes sure the projectile to fire has been properly assigned in the inspector
         if (projectileType == null || shootPoint == null)
         {
             Debug.LogWarning("ProjectileType or ShootPoint not assigned.");
             return;
         }
 
+        //Create the projectile
         Instantiate(projectileType, shootPoint.position, Quaternion.identity);
     }
+
+    //Used for changing attack types.
     public void SetProjectileType(string newAttackType)
     {
         attackType = newAttackType;
